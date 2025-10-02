b17d351e9d37c4743d838dcacd78280bfc6b858a
Papp Dávid <pappdavidlevele@gmail.com>
2025-10-03 00:47:04 +0200

    primeVue added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+import Aura from "@primeuix/themes/aura";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@primevue/nuxt-module"],
 
   css: ["./assets/style.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
   },
 });
