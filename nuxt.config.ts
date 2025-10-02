c2732cef7ed14dfedd77f751f8da07fcc0bbc2ad
Papp Dávid <pappdavidlevele@gmail.com>
2025-10-03 00:36:20 +0200

    tailwind added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+
+  modules: ["@nuxt/eslint"],
+
+  css: ["./assets/style.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
