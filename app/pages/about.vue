4bf795b75579a51945842a9cf70345688479bd21
Papp Dávid <pappdavidlevele@gmail.com>
2025-10-06 03:00:14 +0200

    added text and images

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,7 +1,91 @@
 <template>
-  <div><p>about</p></div>
+  <section class="max-w-3xl mx-auto py-32">
+    <h1>Rólunk</h1>
+    <div class="text-gray-800">
+      <p>
+        Lorem ipsum dolor sit amet, <b>consectetur</b> adipiscing elit. Sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat.
+      </p>
+      <br />
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.
+      </p>
+      <p>
+        Excepteur sint occaecat cupidatat non <b>proident</b>,
+        <i>sunt obou</i> in culpa qui officia deserunt mollit anim id est
+        laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam. Sunt in culpa qui
+        officia deserunt mollit anim id est laborum.
+      </p>
+      <br />
+
+      <div class="flex flex-col md:flex-row items-start gap-6">
+        <div class="w-full md:w-1/3">
+          <img
+            src="https://images.unsplash.com/photo-1563434649554-58f91d22ec2c?q=80&w=879&auto=format&fit=crop&ixlib=rb-4.1.0"
+            alt="Lorem kép"
+            class="w-full h-auto object-cover"
+          />
+        </div>
+
+        <!-- Szöveg -->
+        <div class="w-full md:w-2/3 text-gray-800 space-y-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+      <br></br>
+      <p>
+        Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt obou in
+        culpa qui officia deserunt mollit anim id est laborum. Sed ut
+        perspiciatis unde omnis iste natus error sit voluptatem accusantium
+        doloremque laudantium, totam rem aperiam. Sunt in culpa qui officia
+        deserunt mollit anim id est laborum.
+      </p>
+      <div class="flex flex-col md:flex-row items-start gap-6">
+       
+
+        <!-- Szöveg -->
+        <div class="w-full md:w-2/3 text-gray-800 space-y-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+         <div class="w-full md:w-1/3">
+          <img
+            src="https://images.unsplash.com/photo-1733410027841-09dc77bd1832?q=80&w=830&auto=format&fit=crop&ixlib=rb-4.1.0"
+            alt="Lorem kép"
+            class="w-full h-auto object-cover"
+          />
+        </div>
+      </div> 
+      <br />
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </section>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
