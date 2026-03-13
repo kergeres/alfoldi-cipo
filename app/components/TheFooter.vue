df0d135ee523065edad7fe0cc8b3f8c28dd52c19
Papp Dávid <pappdavidlevele@gmail.com>
2026-03-14 00:26:02 +0100

    fix: footer p color, deploy dot

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer>
     <div class="bg-[var(--primary-brown-100)] px-6 py-8 md:py-14 flex flex-col items-center text-white">
       <img src="@/assets/images/alfoldi_logo_white.png" alt="Logo" class="h-32 md:h-42 w-auto mb-4 filter grayscale" />
-      <p class="text-sm text-white text-center max-w-md">
+      <p class="text-sm !text-white text-center max-w-md">
         Ez most még csak egy példa szöveg, de frappáns lesz, az biztos. Ez most még tényleg csak egy példa szöveg, de frappáns lesz, az biztos.
       </p>
     </div>
